315e0017bb3ae63c7930546c0bd7d7f2caa3367a
Bhavya Yadav <guniverse2006@gmail.com>
2026-07-08 11:03:32 +0530

    Time: 4 ms (52.71%) | Memory: 10.1 MB (31.49%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -8,30 +8,33 @@ public:
             
 
         vector<int> freqs1(26,0);
+        vector<int> freqs2(26,0);
 
         for(int l = 0 ; l < s1.length(); l++ ){
                 freqs1[s1[l] - 'a']++;
+                freqs2[s2[l] - 'a']++;
         }
 
         bool ans = false;
 
+        if(freqs1 == freqs2){
+            ans = true; 
+            
+        }
+
+        
+
         int w = s1.length();
 
-        for(int i =  w; i <= s2.length(); i++){
+        for(int i =  w; i < s2.length(); i++){
             int j = i -w;
-            
-            vector<int> freqs2(26,0);
-
-            
-            for(int l = j ; l < i; l++ ){
-                freqs2[s2[l] - 'a']++;
-            }
-
+            freqs2[s2[i] - 'a']++;
+            freqs2[s2[j] - 'a']--;
             if(freqs1 == freqs2){
                 ans = true; 
                 break;
             }
-            
+
         }
 
 
