5d9c79816fcf5ec9d4c63899b843849f5efebe70
Bhavya Yadav <guniverse2006@gmail.com>
2026-08-31 17:16:52 +0530

    Time: 56 ms (91.04%) | Memory: 43.8 MB (93.61%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -1,53 +1,25 @@
 class Solution {
 public:
 
-    bool rootToNodePath(TreeNode* root, TreeNode* target, 
-                        vector<TreeNode*>& path) {
-        if (root == NULL) {
-            return false;
+    TreeNode* lca(TreeNode* root, TreeNode* p,TreeNode* q){
+        if(root == NULL){
+            return NULL;
+        }
+        if(root == p || root == q){
+            return root;
         }
 
-        path.push_back(root);
+        TreeNode* left = lca(root->left,p,q);
+        TreeNode* right = lca(root->right,p,q);
 
-        if (root == target) {
-            return true;
+        if(left != NULL && right != NULL){
+            return root;
         }
 
-        bool isLeft = rootToNodePath(root->left, target, path);
-        bool isRight = rootToNodePath(root->right, target, path);
-
-        if (isLeft || isRight) {
-            return true;
-        }
-
-        path.pop_back();
-        return false;
-    }
-
-    TreeNode* LCA(TreeNode* root, TreeNode* p, TreeNode* q) {
-        vector<TreeNode*> path1;
-        vector<TreeNode*> path2;
-
-        rootToNodePath(root, p, path1);
-        rootToNodePath(root, q, path2);
-
-        TreeNode* lca = NULL;
-
-        for (int i = 0, j = 0;
-             i < path1.size() && j < path2.size();
-             i++, j++) {
-
-            if (path1[i] != path2[j]) {
-                break;
-            }
-
-            lca = path1[i];
-        }
-
-        return lca;
+        return left == NULL ? right : left;
     }
 
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        return LCA(root, p, q);
+        return lca(root,p,q);
     }
 };
